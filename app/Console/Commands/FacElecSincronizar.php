210003b5a5390cae82f1a5b3bec1e5fc74f050ca
David E. Barrera <dbarrerap@live.com>
2023-08-30 20:02:51 -0500

    Correccion en generacion de clave de acceso

--- app/Console/Commands/FacElecSincronizar.php
+++ app/Console/Commands/FacElecSincronizar.php
@@ -38,13 +38,14 @@ class FacElecSincronizar extends Command
         $bar = $this->output->createProgressBar(count($facturas));
 
         foreach($facturas as $factura) {
+            $secuencial = explode('/', $factura['reference_no']);
             $claveAcceso = \Carbon\Carbon::parse($factura['date'])->format('dmY');
             $claveAcceso .= str_pad('1', 2, '0', STR_PAD_LEFT);  // Tipo Documento: 1 Factura, 6 Guia Remision -- 2 de longitud
             $claveAcceso .= $empresa->ruc;  // RUC de la empresa -- 13 de longitud
             $claveAcceso .= '1'; // Tipo de Ambiente: 1 Desarrollo, 2 Produccion -- 1 de longitud
             $claveAcceso .= str_pad($factura['pos_settings']['cf_value1'], 3, '0', STR_PAD_LEFT);  // Establecimiento -- 3 de longitud
             $claveAcceso .= str_pad($factura['pos_settings']['cf_value2'], 3, '0', STR_PAD_LEFT);  // Punto Emision -- 3 de longitud
-            $claveAcceso .= str_pad($factura['id'], 9, '0', STR_PAD_LEFT);  // Secuencial -- 9 de longitud
+            $claveAcceso .= str_pad($secuencial[3], 9, '0', STR_PAD_LEFT);  // Secuencial -- 9 de longitud
             $claveAcceso .= '12345678'; //Codigo Numerico: segun la ficha tecnica del sri por defecto es 12345678 --- 8 de longitud
             $claveAcceso .= '1'; //Tipo Emision: segun la ficha tecnica del sri el codigo por defecto es 1 --- 1 de longitud
             $claveAcceso .= $this->getMod11Dv($claveAcceso);
